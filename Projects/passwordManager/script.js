9c09376492907b6a7e8cdce976336529e2e7f091
subham-kr-singh <subhamkumarsingh077@gmail.com>
2026-05-16 19:25:20 +0530

    Add initial implementation of password manager with form handling and local storage

---
+++ Projects/passwordManager/script.js
@@ -0,0 +1,33 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const webSiteName = document.getElementById("website-name").value.trim();
+  const userName = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value;
+
+  const data = {
+    website: webSiteName,
+    username: userName,
+    password: password,
+  };
+
+  StoreToLocalStorage(data);
+
+  document.getElementById("website-name").value = "";
+  document.getElementById("username").value = "";
+  document.getElementById("password").value = "";
+});
+
+document.querySelector("form").addEventListener("reset", (event) => {
+  event.preventDefault();
+
+  document.getElementById("website-name").value = "";
+  document.getElementById("username").value = "";
+  document.getElementById("password").value = "";
+});
+
+function StoreToLocalStorage(data) {
+  let existingData = JSON.parse(localStorage.getItem("data")) || [];
+  existingData.push(data);
+  localStorage.setItem("data", JSON.stringify(existingData));
+}
