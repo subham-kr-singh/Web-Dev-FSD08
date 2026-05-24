6bf7927cf1ee38dc33bcdba1fb4195fe38150bc3
subham-kr-singh <subhamkumarsingh077@gmail.com>
2026-05-24 10:41:10 +0530

    Update joke fetching logic and add error display in script.js and index.html

--- Projects/JokeApp/script.js
+++ Projects/JokeApp/script.js
@@ -1,9 +1,11 @@
 const getNewJoke = async () => {
-  const API_URL = "https://v2.jokeapi.dev/joke/Programming";
+  const API_URL = "https://official-joke-api.appspot.com/random_joke";
 
   const response = await fetch(API_URL);
   const data = await response.json();
 
-  document.getElementById("setup").innerText = data.joke || data.setup;
-  document.getElementById("delivery").innerText = data.delivery || "";
+  document.getElementById("setup").innerText = data.setup;
+  document.getElementById("delivery").innerText = data.punchline;
+
+  document.getElementById("error").innerText = data.message || "";
 };
